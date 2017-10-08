1c836c57f4d874c02e61880dff590c73212ba047
Buturuga Alexandru Iulian <buturuga.alexandru.iulian@gmail.com>
2017-10-08 13:43:37 +0300

    Add ui-router and ngMaterial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import * as angular from 'angular';
 import { AppComponent } from './app.component';
+import '@uirouter/angularjs';
+import 'angular-material';
+import 'angular-material/angular-material.scss';
 import './app.scss';
 
 
-export default angular.module('SAP-tsc', [])
-  .component('appRoot', AppComponent)
-  .name;
+export default angular.module('SAP-tsc', [
+  'ui.router',
+  'ngMaterial'
+])
+.component('appRoot', AppComponent)
+.name;
 
