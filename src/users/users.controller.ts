d0a990ecff3f5b230b93ee62a61a9ea0c6d52beb
Buturuga Alexandru Iulian <buturuga.alexandru.iulian@gmail.com>
2017-10-09 06:22:27 +0300

    implement users list

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,17 +4,23 @@ import UsersService, { User } from './users.service';
 export default class UsersController implements angular.IComponentController {
 
   static $inject = [ 'UsersService' ];
-  
+
   private users: User[] = [];
+  private totalPages: number = 0;
 
   constructor(private UsersService: UsersService) {
     this.fetchUsers();
   }
 
+  /**
+   * Fetch users from the server and bind them into component.
+   */
   private fetchUsers(): void {
     this.UsersService.all()
       .then((response: any) => {
-        console.log(response);
+        const {data: {users}, pagination: {page_number}} = response;
+        this.users = users;
+        this.totalPages = page_number;
       });
   }
 
